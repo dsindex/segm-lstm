6748711e1455688de06896a8681cd9f2125ed989
dsindex <hazzling@gmail.com>
2016-06-29 11:58:40 +0900

    -

--- sketch.py
+++ sketch.py
@@ -105,6 +105,8 @@ def RNN(_X, _istate, _weights, _biases):
 
 	# Define a lstm cell with tensorflow
 	lstm_cell = rnn_cell.BasicLSTMCell(n_hidden, forget_bias=1.0)
+	#lstm_cell = rnn_cell.LSTMCell(n_hidden, forget_bias=1.0)
+	#lstm_cell = rnn_cell.GRUCell(n_hidden, forget_bias=1.0)
 	# Split data because rnn cell needs a list of inputs for the RNN inner loop
 	_X = tf.split(0, n_steps, _X) # n_steps splits each of which contains (?, n_hidden)
 	'''
