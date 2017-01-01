4f71272fc0f8bd6962446d4e603d0d1be3e07b9c
Myungchul Shin <hazzling@gmail.com>
2017-01-01 16:33:03 +0900

    Update sketch.py

--- sketch.py
+++ sketch.py
@@ -106,7 +106,7 @@ def RNN(_X, _istate, _weights, _biases):
 	_X = tf.matmul(_X, _weights['hidden']) + _biases['hidden'] # (?, n_hidden)
 
 	# Define a lstm cell with tensorflow
-	lstm_cell = tf.nn.rnn_cell.LSTMCell(n_hidden, forget_bias=1.0)
+	lstm_cell = tf.nn.rnn_cell.LSTMCell(n_hidden, forget_bias=1.0, state_is_tuple=False)
 	# Split data because rnn cell needs a list of inputs for the RNN inner loop
 	_X = tf.split(0, n_steps, _X) # n_steps splits each of which contains (?, n_hidden)
 	'''
