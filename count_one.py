9fd65a29adfa3e5e04aa7e4a7cb17062cb20f92c
dsindex <hazzling@gmail.com>
2018-01-25 11:09:12 +0900

    -

--- count_one.py
+++ count_one.py
@@ -4,11 +4,7 @@ import random
 from random import shuffle
 import tensorflow as tf
 
-# from tensorflow.models.rnn import rnn_cell
-# from tensorflow.models.rnn import rnn
-
-#NUM_EXAMPLES = 10000
-#train_input = ['{0:020b}'.format(i) for i in range(2**20)]
+# data preparation
 NUM_EXAMPLES = 1000
 train_input = ['{0:020b}'.format(i) for i in range(2**10)]
 shuffle(train_input)
@@ -38,36 +34,34 @@ train_output = train_output[:NUM_EXAMPLES]
 
 print "test and training data loaded"
 
-
+# build model
 data = tf.placeholder(tf.float32, [None, 20,1]) #Number of examples, number of input, dimension of each input
 target = tf.placeholder(tf.float32, [None, 21])
 num_hidden = 24
-#cell = tf.nn.rnn_cell.LSTMCell(num_hidden,state_is_tuple=True)
 cell = tf.contrib.rnn.LSTMCell(num_hidden,state_is_tuple=True)
 val, _ = tf.nn.dynamic_rnn(cell, data, dtype=tf.float32)
-print 'before transpose : ', val
+print 'before transpose : ', val.get_shape()
 val = tf.transpose(val, [1, 0, 2])
-print 'after transpose : ', val
-print val.get_shape()
-print int(val.get_shape()[0])
+print 'after transpose : ', val.get_shape()
+print 'first dimension of val : ', int(val.get_shape()[0])
 last = tf.gather(val, int(val.get_shape()[0]) - 1)
 print 'last : ', last.get_shape()
-
 weight = tf.Variable(tf.truncated_normal([num_hidden, int(target.get_shape()[1])]))
 bias = tf.Variable(tf.constant(0.1, shape=[target.get_shape()[1]]))
-prediction = tf.nn.softmax(tf.matmul(last, weight) + bias)
+output = tf.matmul(last, weight) + bias
+print 'output : ', output.get_shape()
+prediction = tf.nn.softmax(output)
+print 'prediction : ', prediction.get_shape()
 cross_entropy = -tf.reduce_sum(target * tf.log(tf.clip_by_value(prediction,1e-10,1.0)))
 optimizer = tf.train.AdamOptimizer()
 minimize = optimizer.minimize(cross_entropy)
 mistakes = tf.not_equal(tf.argmax(target, 1), tf.argmax(prediction, 1))
 error = tf.reduce_mean(tf.cast(mistakes, tf.float32))
 
-#init_op = tf.initialize_all_variables()
+# training
 init_op = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init_op)
-
-#batch_size = 1000
 batch_size = 100
 no_of_batches = int(len(train_input)) / batch_size
 epoch = 5000
@@ -78,6 +72,8 @@ for i in range(epoch):
         ptr+=batch_size
         sess.run(minimize,{data: inp, target: out})
     print "Epoch ",str(i)
+
+# test
 incorrect = sess.run(error,{data: test_input, target: test_output})
 print sess.run(prediction,{data: [[[1],[0],[0],[1],[1],[0],[1],[1],[1],[0],[1],[0],[0],[1],[1],[0],[1],[1],[1],[0]]]})
 print('Epoch {:2d} error {:3.1f}%'.format(i + 1, 100 * incorrect))
